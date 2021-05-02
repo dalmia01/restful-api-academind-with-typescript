d2651037b927e04bf63ff0a8aa91edd8274b5d14
My name <mymail@example.com>
2021-05-02 20:29:14 +0530

    route error handling - if route not found

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,23 @@
 import * as express from "express";
+import { Express, Request, Response, NextFunction } from "express";
 import productsRouter from "./api/routes/products.routes";
 
 import { requestMiddleware } from "./middleware/requests.middleware";
 
-const app = express();
+const app: Express = express();
 
 app.use(express.json());
 app.use(requestMiddleware);
 
 app.use("/products", productsRouter);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    const error = new Error("Route not found");
+    res.status(404);
+    res.json({
+        error: error["message"],
+    });
+});
 
 const PORT = process.env.port || 3000;
 
