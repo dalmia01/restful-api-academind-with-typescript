615566c703cd9b2a0cbf899cc0eb658ee9ca82bf
My name <mymail@example.com>
2021-05-03 23:53:56 +0530

    more graphql crud operations implemented

--- server/src/graphql/resolvers/users.resolvers.ts
+++ server/src/graphql/resolvers/users.resolvers.ts
@@ -1,17 +1,26 @@
 import { Arg, Mutation, Query, Resolver } from "type-graphql";
 import UsersModel from "../../api/models/users.models";
-import { hashPassword } from "../../helpers/common.helpers";
-import { User, UserRegisterInput } from "../schemas/users.schemas";
+import { assignJWTAccessToken, hashPassword, verifyPassword } from "../../helpers/common.helpers";
+import { User, UserError, UserInput, UserTokenResponse, UserResponse, UserResetInput } from "../schemas/users.schemas";
 
 @Resolver((of) => User)
 export class UserResolver {
-    @Query((returns) => String)
+    @Query(() => String)
     async hello(): Promise<string> {
         return "hello graphql world!";
     }
 
-    @Mutation((returns) => User)
-    async signUpUser(@Arg("userRegisterInput") { username, email, password }: UserRegisterInput): Promise<User> {
+    @Query(() => [User])
+    async getAllUsers(@Arg("role", { nullable: true }) role: string): Promise<User[]> {
+        let users = role ? await UsersModel.find({ role }) : await UsersModel.find();
+        console.log(users.length);
+        if (users.length < 1) return [{ error: "no user found" }];
+
+        return users;
+    }
+
+    @Mutation(() => User)
+    async signUpUser(@Arg("UserInput") { username, email, password }: UserInput): Promise<User> {
         const user = await UsersModel.findOne({ username });
 
         if (user) {
@@ -39,5 +48,68 @@ export class UserResolver {
             username: userResult.username,
             email: userResult.email,
         };
+    }
+
+    @Mutation(() => UserTokenResponse)
+    async signInuser(@Arg("userInput") { username, password }: UserInput): Promise<UserTokenResponse> {
+        const user = await UsersModel.findOne({ username });
+
+        if (!user)
+            return {
+                error: "User doesn't exist",
+            };
+
+        const isPasswordVerified = await verifyPassword(password, user.password);
+
+        if (!isPasswordVerified)
+            return {
+                error: "wrong credentials",
+            };
+
+        const jwtAccessToken = assignJWTAccessToken(username, user.id);
+
+        if (!jwtAccessToken)
+            return {
+                error: "Some error occured, while assigning token. try again later!",
+            };
+
+        return {
+            id: user.id,
+            username: user.username,
+            token: jwtAccessToken,
+        };
+    }
+
+    @Mutation(() => UserError)
+    async deleteUser(@Arg("UserInput") { username }: UserInput): Promise<UserError> {
+        const user = await UsersModel.deleteOne({ username });
+
+        if (user.deletedCount < 1)
+            return {
+                error: "wrong credentials!",
+            };
+
+        return {
+            message: "user deleted successfully",
+        };
+    }
+
+    @Mutation(() => UserResponse)
+    async resetUserPassword(@Arg("UserResetInput") { username, password, new_password }: UserResetInput): Promise<UserResponse> {
+        const user = await UsersModel.findOne({ username });
+
+        if (!user) return { error: "user does not exist" };
+
+        const isPasswordVerified = await verifyPassword(password, user.password);
+
+        if (!isPasswordVerified) return { error: "wrong credentials" };
+
+        const hashedPassword = await hashPassword(new_password);
+
+        if (!hashedPassword) return { error: "some error occured while hashing password" };
+
+        const modifiedUser = await UsersModel.findOneAndUpdate({ username }, { password: hashedPassword });
+
+        return { id: modifiedUser.id, username };
     }
 }
--- server/src/graphql/schemas/users.schemas.ts
+++ server/src/graphql/schemas/users.schemas.ts
@@ -1,6 +1,30 @@
 import { Field, ObjectType, InputType } from "type-graphql";
 
 @ObjectType()
+export class UserError {
+    @Field({ nullable: true })
+    message?: string;
+    @Field({ nullable: true })
+    error?: string;
+}
+
+@ObjectType()
+export class UserResponse {
+    @Field({ nullable: true })
+    id?: string;
+    @Field({ nullable: true })
+    username?: string;
+    @Field({ nullable: true })
+    error?: string;
+}
+
+@ObjectType()
+export class UserTokenResponse extends UserResponse {
+    @Field({ nullable: true })
+    token?: string;
+}
+
+@ObjectType()
 export class User {
     @Field({ nullable: true })
     id?: string;
@@ -19,7 +43,7 @@ export class User {
 }
 
 @InputType()
-export class UserRegisterInput implements Partial<User> {
+export class UserInput implements Partial<User> {
     @Field({ nullable: true })
     username?: string;
     @Field({ nullable: true })
@@ -28,4 +52,14 @@ export class UserRegisterInput implements Partial<User> {
     password?: string;
     @Field({ nullable: true })
     profileImage?: string;
+}
+
+@InputType()
+export class UserResetInput {
+    @Field()
+    username: string;
+    @Field()
+    password: string;
+    @Field()
+    new_password: string;
 }
